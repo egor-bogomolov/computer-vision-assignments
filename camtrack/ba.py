f399a8fea6180909cfb329d0ddbe4a8fedc3ac85
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-03-28 16:07:12 +0300

    Update point calibration

--- camtrack/ba.py
+++ camtrack/ba.py
@@ -1,8 +1,9 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 from collections import namedtuple
 import numpy as np
 from scipy.optimize import approx_fprime as derivative
+from time import time
 
 from corners import FrameCorners
 from _camtrack import *
@@ -17,17 +18,23 @@ def run_bundle_adjustment(intrinsic_mat: np.ndarray,
                           list_of_corners: List[FrameCorners],
                           max_inlier_reprojection_error: float,
                           view_mats: List[np.ndarray],
-                          pc_builder: PointCloudBuilder) -> List[np.ndarray]:
-
+                          point_positions: List[np.ndarray]) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+    print('Running bundle adjustment')
+    orig_point_positions = point_positions
+    def transf(p):
+        if p is not None:
+            return p
+        return np.zeros(3)
+    point_positions = np.array([transf(p) for p in point_positions])
     proj_mats = [intrinsic_mat @ view_mat for view_mat in view_mats]
     projection_errors = []
     used_3d_points_inds = set()
 
     for k, (proj_mat, corners) in enumerate(zip(proj_mats, list_of_corners)):
-        indices = np.array([ind for ind in corners.ids if ind in pc_builder.ids], dtype=np.int32)
+        indices = np.array([ind for ind in corners.ids if point_positions[ind] is not None], dtype=np.int32)
         indices_2d_local = np.array([k for k, ind in enumerate(corners.ids) if ind in indices], np.int32)
-        indices_3d_local = np.array([k for k, ind in enumerate(pc_builder.ids) if ind in indices], np.int32)
-        inlier_indices = calc_inlier_indices(pc_builder.points[indices_3d_local],
+        indices_3d_local = indices[:, 0]
+        inlier_indices = calc_inlier_indices(point_positions[indices_3d_local],
                                              corners.points[indices_2d_local],
                                              proj_mat,
                                              max_inlier_reprojection_error)
@@ -41,21 +48,21 @@ def run_bundle_adjustment(intrinsic_mat: np.ndarray,
     point_ind_to_position = {}
     n_matrix_params = 6 * len(view_mats)
     p = np.concatenate([_view_mats3x4_to_rt(view_mats),
-                        _points_to_flat_coordinates(pc_builder.points[used_3d_points_inds])])
+                        _points_to_flat_coordinates(point_positions[used_3d_points_inds])])
     for k, point_ind in enumerate(used_3d_points_inds):
         point_ind_to_position[point_ind] = n_matrix_params + 3 * k
 
-    _run_optimization(projection_errors, list_of_corners, point_ind_to_position, p, intrinsic_mat, 4)
+    if _run_optimization(projection_errors, list_of_corners, point_ind_to_position, p, intrinsic_mat, 3):
+        for k in range(len(view_mats)):
+            r_vec = p[6 * k: 6 * k + 3].reshape(3, 1)
+            t_vec = p[6 * k + 3: 6 * k + 6].reshape(3, 1)
+            view_mat = rodrigues_and_translation_to_view_mat3x4(r_vec, t_vec)
+            view_mats[k] = view_mat
 
-    for k in range(len(view_mats)):
-        r_vec = p[6 * k : 6 * k + 3].reshape(3, 1)
-        t_vec = p[6 * k + 3 : 6 * k + 6].reshape(3, 1)
-        view_mat = rodrigues_and_translation_to_view_mat3x4(r_vec, t_vec)
-        view_mats[k] = view_mat
+        for k, ind in enumerate(used_3d_points_inds):
+            orig_point_positions[ind] = p[n_matrix_params + 3 * k: n_matrix_params + 3 * k + 3]
 
-    pc_builder.update_points(pc_builder.ids[used_3d_points_inds], p[n_matrix_params:].reshape(-1, 3))
-
-    return view_mats
+    return view_mats, orig_point_positions
 
 
 def _view_mats3x4_to_rt(view_mats: List[np.ndarray]) -> np.ndarray:
@@ -110,7 +117,8 @@ def _compute_jacobian(projection_errors: List[ProjectionError],
                       mapping: Dict[int, int],
                       p: np.ndarray,
                       intrinsic_mat: np.ndarray) -> np.ndarray:
-
+    start_time = time()
+    print("Started Jacobian computation")
     J = np.zeros((len(projection_errors), len(p)))
     for row, proj_err in enumerate(projection_errors):
         vec = np.zeros(9)
@@ -132,7 +140,7 @@ def _compute_jacobian(projection_errors: List[ProjectionError],
 
         for i in range(3):
             J[row, point_pos + i] = partial_derivatives[6 + i]
-
+    print(f"Finished in {time() - start_time} sec")
     return J
 
 
@@ -141,32 +149,66 @@ def _run_optimization(projection_errors: List[ProjectionError],
                       mapping: Dict[int, int],
                       p: np.ndarray,
                       intrinsic_mat: np.ndarray,
-                      n_steps: int):
-    n = 6 * len(list_of_corners)
-    lmbd = 10
-    errors = _reprojection_errors(projection_errors, list_of_corners, mapping, p, intrinsic_mat)
-    print(errors.sum())
-    for step in range(n_steps):
+                      n_steps: int) -> bool:
+    n = 3 * len(list_of_corners)
+    lmbd = 2.
+    initial_error = _reprojection_errors(projection_errors, list_of_corners, mapping, p, intrinsic_mat).sum()
+    print(f'Initial error: {initial_error}')
+    if initial_error < 1.:
+        print(f'Error is too small for BA')
+        return False
+
+    total_steps = 0
+    successful_steps = 0
+    hit_the_bottom = False
+    lmbd_change = 4.
+    while successful_steps < n_steps or total_steps < n_steps * 2:
+        total_steps += 1
         J = _compute_jacobian(projection_errors, list_of_corners, mapping, p, intrinsic_mat)
         JJ = J.T @ J
         JJ += lmbd * np.diag(np.diag(JJ))
         U = JJ[:n, :n]
         W = JJ[:n, n:]
         V = JJ[n:, n:]
-        V_inv = np.zeros_like(V)
-        for i in range(0, len(V), 3):
-            s = 3 * i
-            t = 3 * i + 3
-            V_inv[s:t, s:t] = np.linalg.inv(V[s:t, s:t])
+        try:
+            V_inv = np.zeros_like(V)
+            for i in range(0, len(V), 3):
+                s = 3 * i
+                t = 3 * i + 3
+                V_inv[s:t, s:t] = np.linalg.inv(V[s:t, s:t])
+        except np.linalg.LinAlgError:
+            hit_the_bottom = True
+            lmbd *= lmbd_change
+            continue
 
         g = J.T @ _reprojection_errors(projection_errors, list_of_corners, mapping, p, intrinsic_mat)
         A = U - W @ V_inv @ W.T
         b = W @ V_inv @ g[n:] - g[:n]
-        dc = np.linalg.solve(A, b)
+
+        try:
+            dc = np.linalg.solve(A, b)
+        except np.linalg.LinAlgError:
+            hit_the_bottom = True
+            lmbd *= lmbd_change
+            continue
+
         dx = V_inv @ (-g[n:] - W.T @ dc)
 
         p[:n] += dc
         p[n:] += dx
-
-    errors = _reprojection_errors(projection_errors, list_of_corners, mapping, p, intrinsic_mat)
-    print(errors.sum())
+        error = _reprojection_errors(projection_errors, list_of_corners, mapping, p, intrinsic_mat).sum()
+        if error > initial_error:
+            p[:n] -= dc
+            p[n:] -= dx
+            lmbd *= lmbd_change
+            hit_the_bottom = True
+            print(f"Lambda changed to {lmbd}")
+        else:
+            initial_error = error
+            successful_steps += 1
+            if not hit_the_bottom:
+                lmbd /= lmbd_change
+            print(f"Lambda changed to {lmbd}")
+        print(f"Current error {error}")
+    print(f'Final error: {initial_error}')
+    return True
--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -32,8 +32,6 @@ class CameraTracker:
         # print(self._n_frames)
         self._add_cloud_points(frame_ind1, frame_ind2)
         self._tracking()
-        run_bundle_adjustment(self._intrinsic_mat, corner_storage[:10], 1., self.track()[:10],
-                              self.point_cloud_builder())
 
     def _initialization(self):
         poses = []
@@ -62,10 +60,10 @@ class CameraTracker:
 
         essential_mat, mask_essential = cv2.findEssentialMat(correspondences.points_1, correspondences.points_2,
                                                              self._intrinsic_mat,
-                                                             method=cv2.RANSAC, prob=0.999, threshold=1.)
+                                                             method=cv2.RANSAC, prob=0.9999, threshold=1.)
 
         _, mask_homography = cv2.findHomography(correspondences.points_1, correspondences.points_2,
-                                                method=cv2.RANSAC, confidence=0.999,
+                                                method=cv2.RANSAC, confidence=0.9999,
                                                 ransacReprojThreshold=self._triangulation_parameters.max_reprojection_error)
 
         # zeros in mask correspond to outliers
@@ -106,6 +104,7 @@ class CameraTracker:
         self._log_added_points(frame_ind1, frame_ind2, self._add_points(points, ids))
 
     def _tracking(self):
+        ba_step = 5
         added = True
         while added:
             added = False
@@ -121,12 +120,13 @@ class CameraTracker:
                     print(f"Processed frame {i}")
                     added = True
 
-                    if i % 10 == 0:
-                        self._track[i - 9: i + 1] = run_bundle_adjustment(self._intrinsic_mat,
-                                                                          self._corner_storage[i - 9: i + 1],
-                                                                          self._triangulation_parameters.max_reprojection_error,
-                                                                          self._track[i - 9: i + 1],
-                                                                          self.point_cloud_builder())
+                    if i > 0 and i % ba_step == 0:
+                        self._track[i - ba_step + 1: i + 1], self._point_positions = \
+                            run_bundle_adjustment(self._intrinsic_mat,
+                                                  self._corner_storage[i - ba_step + 1: i + 1],
+                                                  self._triangulation_parameters.max_reprojection_error,
+                                                  self._track[i - ba_step + 1: i + 1],
+                                                  self._point_positions)
 
         prev = -1
         for i in range(self._n_frames):
